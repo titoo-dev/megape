071c5578e743673dc26e49f393217162f547c051
titoo-dev <dev.titosy@gmail.com>
2025-09-16 15:33:55 +0300

    refactor: comment out ContactSection for future consideration and maintain cleaner layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
       <ProductsSection />
       <MediaSection />
       <CahierSection />
-      <ContactSection />
+      {/* <ContactSection /> */}
       {/* <StatsSection /> */}
       <FloatingActionButtonServer />
     </>
